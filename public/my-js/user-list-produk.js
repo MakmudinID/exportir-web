eca264eb3403354354297073a3c6877b40e38339
Makmudin <id.makmudin@gmail.com>
2022-10-23 14:02:54 +0700

    update

--- public/my-js/user-list-produk.js
+++ public/my-js/user-list-produk.js
@@ -12,64 +12,10 @@ function filter() {
     })
 }
 
-function add_cart(id, id_umkm, img, produk, qty, harga, weight) {
-    $.ajax({
-        url: base_url + '/add-cart',
-        type: 'POST',
-        data: {
-            id: id,
-            id_umkm: id_umkm,
-            img: img,
-            produk: produk,
-            qty: qty,
-            harga: harga,
-            weight: weight,
-        },
-        dataType: 'JSON',
-        success: function(res) {
-            $('.total-cart').html(res.total);
-            if (res.result != true) {
-                Swal.fire({
-                    title: 'Gagal',
-                    html: "Maaf Anda Belum Login !",
-                    icon: 'error',
-                    timer: 3000,
-                    showCancelButton: false,
-                    showConfirmButton: false,
-                    buttons: false,
-                });
-            } else {
-                Swal.fire({
-                    title: 'Berhasil',
-                    html: "Produk Berhasil Masuk Keranjang",
-                    icon: 'success',
-                    timer: 3000,
-                    showCancelButton: false,
-                    showConfirmButton: false,
-                    buttons: false,
-                });
-            }
-            // console.log(res)
-        }
-    })
-}
-
 jQuery(document).ready(function() {
     filter();
 
     $('#kategori').on('change', function() {
         filter();
-    })
-
-    $(document).on('click', '.add-cart', function(e) {
-        e.preventDefault();
-        var id = $(this).data('id');
-        var umkm = $(this).data('umkm');
-        var img = $(this).data('img');
-        var produk = $(this).data('produk');
-        var qty = $(this).data('qty');
-        var harga = $(this).data('harga');
-        var weight = $(this).data('weight');
-        add_cart(id, umkm, img, produk, qty, harga, weight)
     })
 })
